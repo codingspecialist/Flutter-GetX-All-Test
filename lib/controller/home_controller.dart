e003adb673a4a5754731cce589e41773cd144e24
ssar@nate.com <ssar@nate.com>
2021-07-16 02:22:44 +0900

    onInit() 로그 남기기

--- lib/controller/home_controller.dart
+++ lib/controller/home_controller.dart
@@ -6,6 +6,7 @@ class HomeController extends GetxController {
   // onInit() 함수를 사용하면 HomeController() 객체 생성시 초기화를 할 수 있다.
   @override
   void onInit() {
+    print("HomeController onInit()");
     super.onInit();
     this.data.value = ["data1", "data2", "data3", "data4"];
   }
